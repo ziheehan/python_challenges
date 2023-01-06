4ddbcb87f0e325056845da4040fdc1b918c8ecf9
ziheehan <gkswlgml90@gmail.com>
2023-01-06 13:06:43 +0900

    [Day 3] Challenge Success

---
+++ 008.py
@@ -0,0 +1,3 @@
+total_price = int(input("총 가격은 얼마입니까?"))
+people = int(input("몇 명이 먹었습니까?"))
+print("인당", total_price / people,"원씩 지불해주세요")
---
+++ 009.py
@@ -0,0 +1,5 @@
+days = int(input("날을 입력하세요 : "))
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
+print(days, "일까지는", hours, "시간, ",minutes,"분, ", seconds, "초 남았습니다.")
